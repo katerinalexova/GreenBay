e87fa9b3b8639723793f045c63c781c056921f08
Kateřina Lexová <lexova.katka131@gmail.com>
2023-02-15 15:20:50 +0100

    add folders and entities

---
+++ GreenBay/Models/Entities/Item.cs
@@ -0,0 +1,17 @@
+﻿namespace GreenBay.Models.Entities
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhotoUrl { get; set; }
+        public int Price { get; set; }
+        public User User { get; set; }
+
+        public Item()
+        {
+
+        }
+    }
+}
---
+++ GreenBay/Models/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿namespace GreenBay.Models.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public double Coins { get; set; }
+        public List<Item>? Items { get; set; }
+
+
+        public User()
+        {
+
+        }
+    }
+}
